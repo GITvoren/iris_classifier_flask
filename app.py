de5a3f48b8bad715acfb0975c3fd6342f996c3a3
GITvoren <tejuco.voren@gmail.com>
2023-08-14 09:01:06 +0800

    added flask api

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
+import iris_model
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+CORS(app)
 
-def basic():
-    return jsonify("hello")
+@app.route('/predict', methods=['POST'])
+def predict_specie():
+     sepal_length = request.json['sepallength']
+     sepal_width = request.json['sepalwidth']
+     petal_length = request.json['petallength']
+     petal_width = request.json['petalwidth']
+     y_pred = [[sepal_length, sepal_width, petal_length, petal_width]]
+     trained_model = iris_model.training_model()
+     prediction_value = trained_model.predict(y_pred)
+
+     if prediction_value == 0:
+         return jsonify("Setosa")
+     elif prediction_value == 1:
+         return jsonify("Versicolor")
+     else:
+         return jsonify("Virginica")
 
 
 if __name__ == '__main__':
-     app.run(debug=True)
+     app.run(debug=False)
+ 
--- iris_model.py
+++ iris_model.py
@@ -5,18 +5,18 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Load data file
 data = load_iris()
-print(data)
+# print(data)
 
 # Select independent and dependent variables ( Features/Input, Labels/Target/Output )
 # Organize data and labels w/ DataFrames from pandas
 X = pd.DataFrame(data.data, columns=(data.feature_names))
-Y = pd.DataFrame(data.target, columns=['Target'])
+y = pd.DataFrame(data.target, columns=['Target'])
 
 # Split data set for training and testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.3)
 
 
-# Create model using Decision Tree Algorithm
+# Create/Instantiate model using Decision Tree Algorithm
 # Train Model 'model.fit(X, Y)'
 def training_model():
     model = DecisionTreeClassifier()
